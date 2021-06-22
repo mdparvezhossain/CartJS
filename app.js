7fb3911fc5e933f9e2ac42aa586a5549ef4e30fe
Md Parvez Hossain <80127536+mdparvezhossain@users.noreply.github.com>
2021-06-22 19:25:49 +0600

    cardJsModify

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
-const carts= document.querySelector("#food-sec");
+    const foodSec = document.querySelector("#food-sec");
+    const showCard = document.querySelector('#show-card tbody');
+    const removeAll = document.querySelector('#remove_all');
 
-const addToCart =(e) => {
-    const target = e.target;
-    if(target.classList.contains("btn")) {
-        const cart = target.parentElement.parentElement;
+   function addToCard (event) {
+        event.preventDefault();
+       const target =  event.target;
+    if(target.classList.contains("btn")){
+        const foodCards = target.parentElement.parentElement;
 
-        allcart(cart);
-        countItem(childCart)
+        getFood(foodCards);
+        }
+    };
+    function getFood(foodCard){
+        console.log(foodCard);
+
+        const cardObj = {
+            image: foodCard.querySelector('img').src,
+            title: foodCard.querySelector('.card-title').textContent,
+            itemNo: foodCard.querySelector('.itemNo').textContent,
+            price: foodCard.querySelector('p span').textContent,
+        }
+        displauFoodCard(cardObj)      
+    };
+
+    const displauFoodCard = (cardObj) =>{
+       const tr = document.createElement('tr');
+       tr.innerHTML= `
+       <td>
+            <img src="${cardObj.image}" alt="${cardObj.title}" width="80">
+        </td>
+        <td>
+            <h1 style="font-size:0.8rem">${cardObj.title}</h1>
+        </td>
+        <td>
+            <h1 style="font-size:0.8rem">${cardObj.itemNo}</h1>
+        </td>
+        <td>
+            <h1 style="font-size:0.8rem">${cardObj.price}</h1>
+        </td>
+        <td>
+            <a href="#" class="remove">close</a>
+        </td>
+       `
+       showCard.appendChild (tr);
     }
-}
-function allcart(childCart){
-    console.log(childCart);
+    const removeItem =(event) =>{
+        event.preventDefault();
 
-    const cartObj={
-        image: childCart.querySelector('img').src,
-        title: childCart.querySelector('.card-title').textContent,
-        itemNo: childCart.querySelector('.cardItem').textContent,
-        price: childCart.querySelector('p span').textContent,
-    }
-};
-const countItem = () => {
-    console.log(childCart);
-}
+        const target = event.target;
+        if(target.classList.contains('remove')){
+            const foodCard = target.parentElement.parentElement;
+            foodCard.remove();
+        }
+    };
+    const removeAllItem = () =>{
+        // showCard.innerHTML = " ";
+        while(showCard.firstChild){
+            showCard.removeChild(showCard.firstChild)
+        }
+    };
 
-
-function loadEvents(){
-    carts.addEventListener('click', addToCart)
-}
-loadEvents();
+   function loadEvents(){
+       foodSec.addEventListener("click", addToCard);
+       showCard.addEventListener("click", removeItem);
+       removeAll.addEventListener("click", removeAllItem);
+   }
+   loadEvents();
